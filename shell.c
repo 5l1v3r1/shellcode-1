726142b0995ba66d56d119834b0d13ee181110ab
harryskon <konharrys@gmail.com>
2016-08-24 12:47:41 -0400

    tested on lubuntu

---
+++ shell.c
@@ -0,0 +1,25 @@
+/*
+Contact: Harrys Kon
+Email: konharrys@gmail.com
+https://harryskon.com/
+
+movb $0xb, %al     ;Number of the execve(2) syscall
+int 0x80           ;Execute the syscall
+*/
+
+int main(int argc, char **argv)
+{
+	asm("\
+		shell_start:                 \n\
+		jmp transfer                 \n\
+		code: pop %ebx               \n\
+		xor %eax, %eax               \n\
+		movb $0xb, %al               \n\
+		xor %ecx, %ecx               \n\
+		xor %edx, %edx               \n\
+		int $0x80                    \n\
+		transfer: call code          \n\
+		.string \"/bin/sh\"          \n\
+		shell_end: .octa 0xdeadbeef  \n\
+	");
+}
